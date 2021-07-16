058b5508bc98e717632dd2256201154b0f95be43
DugyalaPoojitha <poojitharaodugyala@gmail.com>
2021-07-16 03:59:46 -0500

    First

---
+++ Fuel1/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using Fuel1.Models.Login;
+using Fuel1.Models.ProfileManagement;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Fuel1.Controllers
+{
+    public class LoginController : Controller
+    {
+        
+        public IActionResult LoginView()
+        {
+            return View("LoginView");
+        }
+
+        public ActionResult RegisterView()
+        {
+            return View("Register");
+        }
+
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+
+            if (model.Email == "Raj_123" && model.Password == "Qwerty!123")
+            {
+                return RedirectToAction("Home1", "Login");
+            }
+            if (ModelState.IsValid)
+            {
+
+                
+
+                if (model.Email=="Raj_123" && model.Password=="Qwerty!123")
+                {
+                    return RedirectToAction("Home1", "Login");
+                }
+
+
+                ModelState.AddModelError(string.Empty, "Loging Failed!");
+
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Register(LoginModel model)
+        {
+            return RedirectToAction("ProfileManage", "Client");
+        }
+        public async Task<IActionResult> ProfileManagement(ProfileManagement model)
+        {
+            return RedirectToAction("Home1", "Login");
+        }
+
+
+        public IActionResult Home1()
+            {
+            return View("Home");
+        }
+
+        
+
+    }
+}
---
+++ Fuel1/Models/ProfileManagement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fuel1.Models.ProfileManagement
+{
+    public class ProfileManagement
+    {
+        [Required(ErrorMessage = "Full Name is required!")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address1 is required!")]
+        [DataType(DataType.Text)]
+        public string Address1 { get; set; }
+
+        [Required(ErrorMessage = "Address2 is required!")]
+        [DataType(DataType.Text)]
+        public string Address2 { get; set; }
+
+        [Required(ErrorMessage = "City is required!")]
+        [DataType(DataType.Text)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "State is required!")]
+        [DataType(DataType.Text)]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "ZipCode is required!")]
+        [DataType(DataType.PostalCode)]
+        public string Zipcode { get; set; }
+
+
+    }
+}
