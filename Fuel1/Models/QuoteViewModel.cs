790e1de0f21fb329b44b125a082c883ec69c1213
anu <anusha.vjcf1@gmail.com>
2021-07-16 13:19:31 -0500

    * QuoteViewModel.cs: First
    * QuoteGenerator.cshtml:
    * Index.cshtml.g.cs:
    * _ViewStart.cshtml.g.cs:
    * Home.cshtml.g.cs:
    * Privacy.cshtml.g.cs:
    * Error.cshtml.g.cs:
    * _ViewImports.cshtml.g.cs:
    * Register.cshtml.g.cs:
    * _Layout.cshtml.g.cs:
    * LoginView.cshtml.g.cs:
    * Register.cshtml.g.cs:
    * LoginView.cshtml.g.cs:
    * ProfileManagement.cshtml.g.cs:
    * _ValidationScriptsPartial.cshtml.g.cs:

---
+++ Fuel1/Models/QuoteViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Fuel1.Models.QuoteViewModel
+{
+    public class QuoteViewModel
+    {
+
+        [Required(ErrorMessage = "Gallons Requested is required!")]
+        [DataType(DataType.Text)]
+        public string GallonsRequested { get; set; }
+
+        [Required(ErrorMessage = "Delivery Address is required!")]
+        [DataType(DataType.Text)]
+        public string DeliveryAddress { get; set; }
+
+        [Required(ErrorMessage = "Date Requested is required!")]
+        [DataType(DataType.Text)]
+        public string DateRequested { get; set; }
+
+        [Required(ErrorMessage = "Price Per Gallon is required!")]
+        [DataType(DataType.Text)]
+        public string PricePerGallon { get; set; }
+
+        [Required(ErrorMessage = "Total Amount Due is required!")]
+        [DataType(DataType.Text)]
+        public string TotalAmount { get; set; }
+
+
+
+    }
+}
